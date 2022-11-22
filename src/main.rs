c2d7a3d9ec9dba3d85de8c33bc5e4d73c8e2ee06
Rahul SIngh <703rahul.singh@gmail.com>
2022-11-22 20:08:24 +0530

    audio setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use rusty_audio::Audio;
+
+fn main() -> Result <(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+
+    for item in &["explode", "lose", "move", "pew", "startup", "win"] {
+        audio.add(item, &format!("sounds/{}.wav", item));
+    }
+    audio.play("startup");
+
+    //cleanup
+    audio.wait();
+    Ok(())
 }
