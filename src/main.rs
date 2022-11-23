33557176fbc1b4452f8c7849e84d55818c41b15b
Rahul SIngh <703rahul.singh@gmail.com>
2022-11-23 20:27:10 +0530

    frame rendering and multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use std::error::Error;
 use rusty_audio::Audio;
+use std::{ io,thread, sync::mpsc::{self, Receiver}};
+use crossterm::{terminal,
+terminal::EnterAlternateScreen,
+terminal::LeaveAlternateScreen,
+ExecutableCommand,
+cursor::Hide,
+cursor::Show,
+event::{self, Event, KeyCode},
+};
+use core::time::Duration;
+use space_invaders::frame::new_frame;
+use space_invaders::render;
 
 fn main() -> Result <(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -9,7 +21,58 @@ fn main() -> Result <(), Box<dyn Error>> {
     }
     audio.play("startup");
 
+    //Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    //render loop in a seperate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true );
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false );
+            last_frame = curr_frame;
+        }
+    });
+
+    //Game Loop
+    'gameloop : loop{
+
+        //per-frame initialization section
+        let curr_frame = new_frame();
+
+        //input events poll
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => { 
+                        audio.play("lose");
+                        break 'gameloop;
+                    },
+                    _ => {}
+                }
+            }
+        }
+
+        //Draw and render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
     //cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
---
+++ src/render.rs
@@ -0,0 +1,26 @@
+use std::io::Stdout;
+use crate::frame::Frame;
+use crossterm::QueueableCommand;
+use crossterm::style::{SetBackgroundColor, Color};
+use crossterm::terminal::{Clear, ClearType};
+use crossterm::cursor::MoveTo;
+use std::io::Write;
+
+pub fn render(stdout : &mut Stdout, last_frame : &Frame, curr_frame: &Frame, force:bool) {
+    if force {
+        stdout.queue(SetBackgroundColor(Color::Blue)).unwrap();
+        stdout.queue(Clear(ClearType::All)).unwrap();
+        stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
+    }
+
+    // x gives the interation index and col gives the value due to enumerate
+    for (x, col) in curr_frame.iter().enumerate() {
+        for (y, s) in col.iter().enumerate() {
+            if *s != last_frame[x][y] || force {
+                stdout.queue(MoveTo(x as u16, y as u16)).unwrap();
+                print!("{}", *s);
+            }
+        }
+    }
+    stdout.flush().unwrap();
+}
